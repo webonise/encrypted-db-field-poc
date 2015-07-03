2479ce73be49b7196251f90ae325f62fce055168
Cesar Valverde G <cdvalverdeg@gmail.com>
2015-07-03 16:10:32 -0600

    Added the Cipher Text Size formula test

--- src/test/java/com/webonise/hibernate/encryptedfields/tests/DaoTest.java
+++ src/test/java/com/webonise/hibernate/encryptedfields/tests/DaoTest.java
@@ -3,6 +3,7 @@ package com.webonise.hibernate.encryptedfields.tests;
 import com.webonise.hibernate.encryptedfields.*;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.crypto.Cipher;
@@ -30,10 +31,30 @@ public class DaoTest {
     private static Logger logger = Logger.getLogger(DaoTest.class.getName());
 
     private static ProjectProperties projectProperties = null;
+    private static SecretKeySpec secretKey = null;
 
     @BeforeClass
     public static void setUp(){
+
         projectProperties = new ProjectProperties();
+
+        //Set up key
+        String myKey  = projectProperties.readProperty(ProjectProperties.AES_KEY);
+
+        byte[] key = null;
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16); // use only first 128 bit
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException e) {
+            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
+        } catch (UnsupportedEncodingException e) {
+            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
+        }
+
     }
 
     /**
@@ -65,7 +86,6 @@ public class DaoTest {
         //Compare
         assertEquals(testEntity2.getField(), testEntity1.getField());
 
-        dao.commitTransaction();
     }
 
     /**
@@ -94,12 +114,7 @@ public class DaoTest {
          */
 
         //Get a connection to the database
-        Connection con = null;
-        try {
-            con =  JDBC_Connection.getConnection();
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Context: DaoTest.testInsertAndReadManually", e);
-        }
+        Connection con = getConnection();
 
         //Connection is required
         assertNotNull(con);
@@ -123,39 +138,11 @@ public class DaoTest {
         //Column is required
         assertNotNull(column);
 
-        String myKey  = projectProperties.readProperty(ProjectProperties.AES_KEY);
-        //Key is required
-        Assert.assertNotNull(myKey);
-
-        //Decrypt
-        String decryptedColumn = null;
-
-        SecretKeySpec secretKey = null;
-        byte[] key = null;
-
-        MessageDigest sha = null;
-        try {
-            key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16); // use only first 128 bit
-            secretKey = new SecretKeySpec(key, "AES");
-        } catch (NoSuchAlgorithmException e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
-        } catch (UnsupportedEncodingException e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
-        }
-
         //secretKey is required
         Assert.assertNotNull(secretKey);
 
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            decryptedColumn = new String(cipher.doFinal(Base64.getDecoder().decode(column)));
-        } catch (GeneralSecurityException e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.decrypt", e);
-        }
+        //Decrypt
+        String decryptedColumn = manualCipherDecrypt(base64Decode(column));
 
         //decryptedColumn is required
         Assert.assertNotNull(decryptedColumn);
@@ -177,54 +164,20 @@ public class DaoTest {
         // Insert Entity
 
         //Get a connection to the database
-        Connection con = null;
-        try {
-            con =  JDBC_Connection.getConnection();
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Context: DaoTest.testInsertAndReadManually2", e);
-        }
+        Connection con = getConnection();
 
         //Connection is required
         assertNotNull(con);
 
-        String myKey  = projectProperties.readProperty(ProjectProperties.AES_KEY);
-        //Key is required
-        Assert.assertNotNull(myKey);
-
-        //Encrypt
-        String encryptedColumn = null;
-
-        SecretKeySpec secretKey = null;
-        byte[] key = null;
-
-        MessageDigest sha = null;
-        try {
-            key = myKey.getBytes("UTF-8");
-            sha = MessageDigest.getInstance("SHA-1");
-            key = sha.digest(key);
-            key = Arrays.copyOf(key, 16); // use only first 128 bit
-            secretKey = new SecretKeySpec(key, "AES");
-        } catch (NoSuchAlgorithmException e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
-        } catch (UnsupportedEncodingException e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.setKey", e);
-        }
-
         //secretKey is required
         Assert.assertNotNull(secretKey);
 
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            encryptedColumn =  Base64.getEncoder().encodeToString(cipher.doFinal((testString).getBytes()));
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Context: Cipher_AES.encrypt", e);
-        }
-
+        String encryptedColumn = base64Encode(manualCipherEncrypt(testString));
         //encryptedColumn is required
+
         Assert.assertNotNull(encryptedColumn);
 
-        //Get column from the database directly
+        //Insert column to the database directly
         Statement stmt = null;
         String sql = "INSERT INTO " + TestEntity.TABLE + " (" + TestEntity.FIELD + ") VALUES ('" + encryptedColumn + "')";
         try {
@@ -248,4 +201,113 @@ public class DaoTest {
         assertEquals(testString, testEntity.getField());
 
     }
+
+    /**
+     * Test of the formula to calculate the size of the cipher text in the database
+     */
+    @Test
+    public void definedSizeTest(){
+
+        String testString = "00000000000000000000000000000000";
+
+        System.out.println("Text size: " + testString.length());
+
+        //secretKey is required
+        Assert.assertNotNull(secretKey);
+
+        /*
+            Using AES algorithm
+         */
+        byte[] encryptedText = manualCipherEncrypt(testString);
+        int encryptedTextSize = encryptedText.length;
+        System.out.println("Encrypted Text size: " + encryptedTextSize);
+
+        String encodedText = base64Encode(encryptedText);
+        int encodedTextSize = encodedText.length();
+        System.out.println("Encoded Text size: " + encodedTextSize);
+
+        /*
+            Using Formula
+         */
+        int calcEncryptedTextSize = calcSizeCipherText(testString.length());
+        System.out.println("Calculated Encrypted Text size: " + calcEncryptedTextSize);
+
+        int calcEncodedTextSize = calcSizeCipherTextBase64Encoded(encryptedText.length);
+        System.out.println("Calculated Cipher Base64 Encoded Text size: " + calcEncodedTextSize);
+
+        //Must be the same
+        Assert.assertEquals(encryptedTextSize, calcEncryptedTextSize);
+
+        //Must be the same
+        Assert.assertEquals(encodedTextSize, calcEncodedTextSize);
+
+    }
+
+    public int calcSizeCipherText(int textSize){
+        return textSize + 16 - (textSize%16);
+    }
+
+    public int calcSizeCipherTextBase64Encoded(int numBytes){
+        return (numBytes + 2 - ((numBytes + 2) % 3)) / 3 * 4;
+    }
+
+    /**
+     * @param text to decode
+     * @return decoded bytes
+     */
+    public byte[] base64Decode(String text){
+        return Base64.getDecoder().decode(text);
+    }
+
+    /**
+     * @param text to encode
+     * @return Base64 encoded String
+     */
+    public String base64Encode(byte[] text){
+        return Base64.getEncoder().encodeToString(text);
+    }
+
+    /**
+     * @param textBase64Encoded
+     * @return the decrypted text
+     */
+    public String manualCipherDecrypt(byte[] textBase64Encoded){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(textBase64Encoded));
+        } catch (GeneralSecurityException e) {
+            logger.log(Level.SEVERE, "Context: Cipher_AES.decrypt", e);
+        }
+        return null;
+    }
+
+    /**
+     * @param text to encrypt
+     * @return Encrypted text
+     */
+    public byte[] manualCipherEncrypt(String text){
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return cipher.doFinal((text).getBytes());
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Context: Cipher_AES.encrypt", e);
+        }
+        return null;
+    }
+
+    /**
+     * @return JDBC database connection
+     */
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection =  JDBC_Connection.getConnection();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Context: DaoTest.getConnection", e);
+        }
+        return connection;
+    }
+
 }
